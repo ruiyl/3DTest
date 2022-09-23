031c5d0d07b219a40c1afde6a0d7164df7d43764
ruiyl <ruiyl1@hotmail.com>
2022-09-23 12:34:21 +0100

    feat: set up player control

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Assets.Scripts
+{
+	public class Player : MonoBehaviour
+	{
+		[SerializeField] private float speed;
+
+		private Rigidbody rb;
+		private Vector2 inputDir;
+
+		private void Awake()
+		{
+			rb = GetComponent<Rigidbody>();
+		}
+
+		private void FixedUpdate()
+		{
+			rb.velocity = speed * ((transform.forward * inputDir.y) + (transform.right * inputDir.x));
+		}
+
+		public void Move(InputAction.CallbackContext context)
+		{
+			if (context.phase == InputActionPhase.Performed || context.phase == InputActionPhase.Canceled)
+			{
+				Vector2 value = context.ReadValue<Vector2>();
+				inputDir = value;
+			}
+		}
+	}
+}
